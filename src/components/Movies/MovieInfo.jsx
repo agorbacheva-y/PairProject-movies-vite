612339470c9c8017a4e5bc59cf64b395cffddeef
agorbacheva-y <agorbacheva.y@gmail.com>
2023-11-08 14:53:19 +0100

    add comments

--- src/components/Movies/MovieInfo.jsx
+++ src/components/Movies/MovieInfo.jsx
@@ -2,6 +2,9 @@
 // useParams
 
 const MovieInfo = () => {
+  const backdropPath = "https://image.tmdb.org/t/p/w1280";
+  // src url for backdrop img is {backdropPath + movies.backdrop_path}
+
   //const movieDetailUrl = `https://api.themoviedb.org/3/movie/${movie_id}?api_key=${api_key}&language=en-US`;
 
   return (
