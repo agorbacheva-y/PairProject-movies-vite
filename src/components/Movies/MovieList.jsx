ddd42daca084763c02651fe86cbb68099f49c4bd
agorbacheva-y <agorbacheva.y@gmail.com>
2023-11-08 14:47:08 +0100

    add alt in img

--- src/components/Movies/MovieList.jsx
+++ src/components/Movies/MovieList.jsx
@@ -1,19 +1,18 @@
-// .map movies and add Link to images that will take user to MovieInfo.jsx
 import { Link } from "react-router-dom";
 const MovieList = ({ movies }) => {
   const posterPath = `https://image.tmdb.org/t/p/w185`;
-  
+
   return (
     <div>
-      {movies?.map(({title, id, poster_path}) => {
+      {movies?.map(({ title, id, poster_path }) => {
         return (
           <Link
             to={`/movie/${title.toLowerCase().replace(/ /g, "%20")}`}
             key={id}
           >
-            <img src={posterPath + poster_path} />
+            <img src={posterPath + poster_path} alt={title} />
           </Link>
-        )
+        );
       })}
     </div>
   );
