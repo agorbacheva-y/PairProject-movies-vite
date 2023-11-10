a5342357d369feecd5506c2ee6b7cbe12e1d85fd
agorbacheva-y <agorbacheva.y@gmail.com>
2023-11-10 22:26:11 +0100

    style not found pg

--- src/Pages/NotFound.jsx
+++ src/Pages/NotFound.jsx
@@ -1,9 +1,22 @@
+import { useNavigate } from "react-router-dom";
 import "./Movies/Movies.css";
+import errorPg from "../assets/errorPg.jpg";
 
 const NotFound = () => {
+  // navigate back to home pg
+  const navigate = useNavigate();
+
   return (
-    <div className="notFound">
-      Movie not found
+    <div 
+      className="container"
+      style={{ backgroundImage: `url(${errorPg})`}}
+    >
+      <div className="notFound">
+        <h1>Sorry, we can't find that film</h1>
+        <p>You'll find lots to explore on the home page.</p>
+        <button onClick={() => navigate("/") }>Home</button>
+      </div>
+      
     </div>
   );
 };
