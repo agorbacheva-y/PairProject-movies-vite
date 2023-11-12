c1ade1475aa1b3476c9e9b3c3889a2126ede0f7b
Michael Holm <michael@weblovers.se>
2023-11-12 23:57:44 +0100

    Added small arrow to home

--- src/components/GenreTags.jsx
+++ src/components/GenreTags.jsx
@@ -46,7 +46,7 @@ export const GenreTags = () => {
        <BurgerMenuIcon />
       </div>
       <div className={`genreTags ${isOpen ? 'open' : ''}`}>
-       <NavLink className="genreTag" to="/">Home</NavLink>
+       <NavLink className="genreTag" to="/">&lt; Home</NavLink>
         {isLoading ? (
           <span>Loading...</span>
         ) : (
