1fa8a92952babb41b289b79c3d8a7f42656a8663
agorbacheva-y <agorbacheva.y@gmail.com>
2023-11-12 22:43:54 +0100

    style attribute

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -6,13 +6,25 @@ const About = () => {
     <div className="aboutPg">
       <h1>Who We Are</h1>
       <div className="aboutImg">
-        <img src={aboutPg}/>
+        <img src={aboutPg} />
       </div>
-      <p>This is a free app for movie-lovers where you can find 
-          details about movies from different genres.
-
-          Click on the genre to filter for the type of movie you want to search.
+      <p>
+        This is a free app for movie-lovers where you can find details about
+        movies from different genres. Click on the genre to filter for the type
+        of movie you want to search.
       </p>
+      <div className="attribute">
+        <p>
+          Photo by{" "}
+          <a href="https://unsplash.com/@fkaregan?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">
+            {" "}Samuel Regan-Asante
+          </a>{" "}
+          on{" "}
+          <a href="https://unsplash.com/photos/the-walking-dead-dvd-movie-wMkaMXTJjlQ?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">
+            Unsplash
+          </a>
+        </p>
+      </div>
     </div>
   );
 };
--- src/Pages/NotFound.jsx
+++ src/Pages/NotFound.jsx
@@ -7,16 +7,24 @@ const NotFound = () => {
   const navigate = useNavigate();
 
   return (
-    <div 
-      className="container"
-      style={{ backgroundImage: `url(${errorPg})`}}
-    >
+    <div className="container" style={{ backgroundImage: `url(${errorPg})` }}>
       <div className="notFound">
         <h1>Sorry, we can't find that film</h1>
         <p>You'll find lots to explore on the home page.</p>
-        <button onClick={() => navigate("/") }>Home</button>
+        <button onClick={() => navigate("/")}>Home</button>
       </div>
-      
+      <div className="attribute">
+        <p>
+          Photo by{" "}
+          <a href="https://unsplash.com/@felixmooneeram?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">
+            Felix Mooneeram
+          </a>{" "}
+          on{" "}
+          <a href="https://unsplash.com/photos/red-cinema-chair-evlkOfkQ5rE?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">
+            Unsplash
+          </a>
+        </p>
+      </div>
     </div>
   );
 };
