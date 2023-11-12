292974f9a55360ca778ecc73f9bf49c808713387
agorbacheva-y <agorbacheva.y@gmail.com>
2023-11-12 21:53:56 +0100

    text to about pg

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,7 +1,12 @@
 const About = () => {
   return (
     <div>
-      about pg
+      <h1>Who We Are</h1>
+      <p>This is a free app for movie-lovers where you can find 
+          details about movies from different genres.
+
+          Click on the genre to filter for the type of movie you want to search.
+      </p>
     </div>
   );
 };
